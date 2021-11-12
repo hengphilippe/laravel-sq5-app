893acd28098506f143b9da5e99a3dcbd7ae6a87e
Bona-Orn <ornbona861@gmail.com>
2021-11-12 20:02:13 +0700

    testing

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body style="background-color: #666666;">
+<h1>Test</h1>
+</body>
+</html>
